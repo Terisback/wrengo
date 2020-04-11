7da9c727b43b1e4aa8f76ba4b03e1abe945a8e51
Terisback <terisbackno@gmail.com>
2020-04-11 16:01:01 +0500

    More comments in code

--- cmd/feature_test/main.go
+++ cmd/feature_test/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"github.com/Terisback/wrengo"
 	"fmt"
+
+	"github.com/Terisback/wrengo"
 )
 
 func main() {
+	// Creating new VM
 	config := wrengo.NewConfiguration()
 	vm := wrengo.NewVM(config)
-	fmt.Println(vm.Interpret("my_module", "System.print(\"I am running in a VM!\")"))
+	defer vm.FreeVM()
+
+	// Interpret "Hello World!" in Wren
+	// It will print in console by default
+	result := vm.Interpret("my_module", "System.print(\"Hello World!\")")
+
+	// Will output RESULT_SUCCESS if it works
+	fmt.Println(result.String())
 }
