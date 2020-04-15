db9a523475299620004392ccb21044beeb83c890
Terisback <terisbackno@gmail.com>
2020-04-15 14:32:56 +0500

    Remove not implemented default callbacks

--- callbacks.go
+++ callbacks.go
@@ -2,29 +2,12 @@ package wrengo
 
 import "fmt"
 
-func CallbackResolveModule(vm *VM, importer, name string) string {
-	fmt.Println("CallbackLoadModule is not implemented")
-	return ""
-}
-
-func CallbackLoadModule(vm *VM, module string) string {
-	fmt.Println("CallbackLoadModule is not implemented")
-	return ""
-}
-
-func CallbackBindForeignMethod(vm *VM, module, className string, isStatic bool, signature string) func(vm *VM) {
-	fmt.Println("BindForeignMethod is not implemented")
-	return func(vm *VM) {}
-}
-
-func CallbackBindForeignClass(vm *VM, module, className string) {
-	fmt.Println("BindForeignClass is not implemented")
-}
-
+// Default write callback that outputs to Stdout
 func CallbackWrite(vm *VM, text string) {
 	fmt.Print(text)
 }
 
+// Default error callback that outputs to Stdout
 func CallbackError(vm *VM, errorType ErrorType, module string, line int, message string) {
 	fmt.Println(fmt.Sprint(errorType.String(), " (", module, ") Line ", line, " : ", message))
 }
